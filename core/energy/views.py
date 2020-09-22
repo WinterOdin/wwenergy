cf1553f607512137381a430c67f6b59cf1e18ce9
Marcel Czuryszkiewicz <45894369+WinterOdin@users.noreply.github.com>
2020-09-22 17:28:31 +0200

    added first frontedn

---
+++ core/energy/views.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta, date
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.templatetags.static import static
+from .models import *
+
+def home(request):
+   
+    context={
+      
+    }
+    return render(request,'index.html', context)
