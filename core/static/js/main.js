8c5600f53650c8214bba25cf7e4afe3e52d1a104
WinterOdin <cczuryszkiewicz@gmail.com>
2020-10-22 23:22:15 +0200

    added forms

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -10,3 +10,21 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlay--active");
 });
+
+(function($) {
+
+
+
+
+$(".open").hide();
+$('.faqQuestion ').click(function(){
+  $(this).next().slideToggle();
+});
+
+$('.clickFaq').click(function(){
+  $('.number').addClass('.numberColor');
+});
+
+
+
+})(jQuery);
