f28f442a546e17c2e3e807cc0578a3472a030443
WinterOdin <cczuryszkiewicz@gmail.com>
2020-10-24 14:31:42 +0200

    gallery via django

--- core/energy/models.py
+++ core/energy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class galleryModel(models.Model):
+    
+    place    = models.CharField(max_length=50, null=False,blank=False)
+    picPlace = models.ImageField(null=True)
+    kwPlace  = models.DecimalField(null=True,blank=False,decimal_places=1,max_digits=6)
+
+    def __str__(self):
+        return self.place + " " +str(self.kwPlace) + " kW"
--- core/energy/views.py
+++ core/energy/views.py
@@ -5,8 +5,12 @@ from django.templatetags.static import static
 from .models import *
 
 def home(request):
-   
+    latest_pics = galleryModel.objects.all()[:6]
+
+
+
+    
     context={
-      
+      'latest_pics':latest_pics,
     }
     return render(request,'index.html', context)
